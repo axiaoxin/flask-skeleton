7db30737e40ef8dc0336c1cbc2a75e4d9150b171
axiaoxin <254606826@qq.com>
2018-07-06 10:47:31 +0800

    Update upgrade.sh

--- deploy/upgrade.sh
+++ deploy/upgrade.sh
@@ -1,34 +1,35 @@
 #!/usr/bin/env bash
-proj_base_dir=/path/to/flask-skeleton
+proj_base_dir=/path/to/deploy
 proj_name=flask-skeleton
 
 current_proj=$proj_base_dir/$proj_name
 
-update_file=$proj_base_dir/$proj_name.tar.gz
+upgrade_file=$proj_base_dir/$proj_name.tar.gz
 backup_proj=$current_proj.`date '+%Y-%m-%d_%H:%M:%S'`
 
 
-cd $proj_base_dir
 
-if [ -f $update_file ]; then
-    # stop current process
-    cd $current_proj/deploy && ./admin.sh stop all && kill $(cat ./supervisor/pids/supervisord.pid)
-    cd $proj_base_dir
-
+if [ -f $upgrade_file ]; then
     # backup current proj
-    mv $current_proj $backup_proj
+    echo backuping...
+    cp -r $current_proj $backup_proj
 
-    # update
-    tar xzf $update_file
+    # update code
+    echo updating code...
+    tar xzf $upgrade_file
+    echo updating settings...
     cp $backup_proj/app/.env $current_proj/app/.env
+    $current_proj/deploy/admin.sh update
+    echo updating requirements...
+    $current_proj/deploy/venv/bin/pip -r $current_proj/requirements.txt
 
-    # start process
-    cd $current_proj/deploy && ./init.sh
-    cd $proj_base_dir
+    # restart
+    echo restarting all...
+    $current_proj/deploy/admin.sh restart all
 
-    # delete old proj
+    # delete old backup
+    echo deleting old backup...
     ls | grep `date '+%Y-%m-%d' --date '30 days ago'` | xargs rm -rf
 else
-    echo no $zip_file
+    echo no $upgrade_file
 fi
-
