20df6451e49afe6e2bd72c9f147d22b874a0a7ca
axiaoxin <254606826@qq.com>
2018-08-08 19:52:42 +0800

    update ratelimit to allow get limit/per dynamic

--- app/utils/request.py
+++ app/utils/request.py
@@ -24,10 +24,10 @@ def celery_task_request_get(key):
     if current_task._get_current_object() is not None:
         return current_task.request.get(key, None)
 
-
 ##############
 # REQUEST ID #
 ##############
+
 
 def x_request_id():
     request_id = request.headers.get(settings.REQUEST_ID_KEY, '').strip()
@@ -54,10 +54,10 @@ def celery_persist_request_id(headers, **kwargs):
 # Forward request_id to celery tasks
 signals.before_task_publish.connect(celery_persist_request_id)
 
-
 ##############
 # RATE LIMIT #
 ##############
+
 
 class RateLimit(object):
     expiration_window = 2
@@ -85,27 +85,33 @@ def on_over_limit(limit):
     return response(code=RetCode.RATE_LIMIT_ERROR)
 
 
-def ratelimit(limit, per, send_x_headers=True,
-              over_limit=on_over_limit,
+def ratelimit(counts=settings.DEFAULT_REQUEST_RATELIMIT_COUNTS,
+              seconds=settings.DEFAULT_REQUEST_RATELIMIT_SECONDS,
+              send_x_headers=True,
+              on_over_limit=on_over_limit,
               scope_func=lambda: request.remote_addr,
               key_func=lambda: request.endpoint):
     '''usage:
-    @ratelimit(limit=100, per=60)
+    @ratelimit(counts=100, seconds=60)
     def view():
        pass
     '''
 
     def decorator(f):
         def rate_limited(*args, **kwargs):
+            limit = counts if isinstance(counts, (int, float)) else counts()
+            per = seconds if isinstance(seconds, (int, float)) else seconds()
             # < 1 表示不限制
             if limit < 1 or per < 1:
                 return f(*args, **kwargs)
 
-            key = 'ratelimit:%s:%s:' % (key_func(), scope_func())
-            rlimit = RateLimit(key, limit, per, send_x_headers)
+            sign_key = 'ratelimit:sign:%s:%s:' % (key_func(), scope_func())
+            rlimit = RateLimit(sign_key, limit, per, send_x_headers)
             g._view_rate_limit = rlimit
-            if over_limit is not None and rlimit.over_limit:
-                return over_limit(rlimit)
+            if on_over_limit is not None and rlimit.over_limit:
+                return on_over_limit(rlimit)
             return f(*args, **kwargs)
+
         return update_wrapper(rate_limited, f)
+
     return decorator
